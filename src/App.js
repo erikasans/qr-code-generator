1f9e5ea130d6ade32922d768870e0fa7b2ea21b3
Erika Sousa <erikasousash.12@gmail.com>
2022-08-22 20:44:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode'
+import './App.css';
+
+function App() {
+  const [link, setLink] = useState('');
+  const [qrcodeLink, setQrcodeLink] = useState('');
+
+
+  function handleGenerate(link_url){
+    QRCodeLink.toDataURL(link_url,{
+      width: 600,
+      margin: 3,
+    }, function (err, url){
+      setQrcodeLink(url)
+    })
+  }
+
+  function handleQrcode(event){
+    setLink(event.target.value);
+    handleGenerate(event.target.value)
+
+  }
+  return (
+    <div className="container">
+
+    <QRCode
+      value= {link}></QRCode>
+    
+
+      <input  className='input'
+      placeholder="Digite seu link..."
+      value={link}
+      onChange={ (event) => handleQrcode(event)}
+      ></input>
+
+      <a className="link" href={qrcodeLink} download={'qcode.png'}>Baixar QrCode</a>
+    </div>
+  );
+}
+
+export default App;
